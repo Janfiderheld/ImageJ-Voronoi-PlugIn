e5c00817f061b504732fa14705574f65c2fe6369
Janfiderheld <Janfitoe@web.de>
2020-08-12 18:10:04 +0200

    Voronoi-Algorithm
    + algorithm to match each pixel to its nearest data point
    + color each pixel accordingly
    ~ refactoring of array list

--- src/main/java/com/mycompany/imagej/Voronoi_PlugIn.java
+++ src/main/java/com/mycompany/imagej/Voronoi_PlugIn.java
@@ -7,7 +7,10 @@ import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
 public class Voronoi_PlugIn implements PlugInFilter {
-	private final int NOT_RED_VAL = 255; 
+	private final int WHITE = 255; 
+	private final int BLACK = 0;
+	
+	private ArrayList<DataPoint> dataPoints = new ArrayList<DataPoint>();
 	
     @Override    
     public int setup(String args, ImagePlus im) {  
@@ -16,41 +19,56 @@ public class Voronoi_PlugIn implements PlugInFilter {
     }
 
     @Override
-    public void run(ImageProcessor ip) {
-    	ArrayList<DataPoint> dataPoints = FindDataPoints(ip);
-    	if(dataPoints == null) {
+    public void run(ImageProcessor ip) {    
+    	findDataPoints(ip);
+    	if(dataPoints.isEmpty()) {
     		return;
     	}
     	
+    	for(int u = 0; u < ip.getWidth(); u++) {
+    		for(int v = 0; v < ip.getHeight(); v++) {
+    			int[] rgb = ip.getPixel(u, v, null);
+    			if(rgb[0] == BLACK && rgb[1] == BLACK && rgb[2] == BLACK) {
+    				continue;
+    			}
+    			
+    			double shortest = Double.MAX_VALUE;
+    			DataPoint temp = null;    			
+    			for(DataPoint count : dataPoints)
+    			{
+    				double dist = count.calculateDistance(u,  v);
+    				if(dist < shortest) {
+    					shortest = dist;
+    					temp = count;
+    				}
+    			}
+    			
+    			if(temp == null) {
+    				IJ.error("No nearest data point was found. Aborting...");
+    				return;
+    			}
+    			
+    			ip.putPixel(u, v, temp.getColor());
+    		}
+    	}
     }
     
     /**
      * Searches all data points in the image.
-     * Definition: All red pixels
+     * Definition of data point: pixel with red colouring
      * @param ip Image to search in
-     * @return ArrayList containing all found data points
      */
-    private ArrayList<DataPoint> FindDataPoints(ImageProcessor ip) {
-    	IJ.log("Start data point search...");
-    	
-    	ArrayList<DataPoint> points = new ArrayList<>();
+    private void findDataPoints(ImageProcessor ip) {
     	for(int u = 0; u < ip.getWidth(); u++) {
     		for(int v = 0; v < ip.getHeight(); v++) {
     			int[] rgb = ip.getPixel(u, v, null);
-    			if(	rgb[0] == NOT_RED_VAL &&
-					rgb[1] != NOT_RED_VAL && 
-					rgb[2] != NOT_RED_VAL) {
-    				points.add(new DataPoint(u, v));
+    			if(rgb[0] == WHITE && rgb[1] != WHITE && rgb[2] != WHITE) {
+    				dataPoints.add(new DataPoint(u, v));
     			}
     		}
     	}
 
-		IJ.log("Search Finished. Found Data Points: " + points.size());
-    	if(points.isEmpty()) {
-    		return null;
-    	}
-    	
+    	IJ.log("Search Finished. Found Data Points: " + dataPoints.size());    	
     	//points.forEach(dp -> IJ.log("x: " + dp.getPosX() + " y: " + dp.getPosY()));
-    	return points;
     }
 }
