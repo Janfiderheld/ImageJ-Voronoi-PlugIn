43c9a16d165150ed3779f3970926896ea0270d36
Janfiderheld <Janfitoe@web.de>
2020-08-12 18:09:05 +0200

    distance calculation & color
    + calculate euclidian distance to a data point
    + generate random color for each data point

--- src/main/java/com/mycompany/imagej/DataPoint.java
+++ src/main/java/com/mycompany/imagej/DataPoint.java
@@ -1,12 +1,19 @@
 package com.mycompany.imagej;
 
+import ij.IJ;
+
 public class DataPoint {
+	private final int WHITE = 255; 
+	private final int BLACK = 0;
+	
 	private int posX;
 	private int posY;
+	private int[] color = new int[3];
 	
 	DataPoint(int x, int y) {
 		setPosX(x);
 		setPosY(y);
+		generateColor();
 	}
 	
 	public int getPosY() {
@@ -24,4 +31,23 @@ public class DataPoint {
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}	
+	
+	public int[] getColor() {
+		return color;
+	}
+	
+	private void generateColor() {
+		for(int i = 0; i < color.length; i++) {
+			int c = (int)(Math.random() * (WHITE - BLACK + 1) + BLACK);
+			IJ.log("Color " + i + " for point (" + posX + ", " + posY + "): " + c);
+			color[i] = c;
+		}
+	}
+	
+	public double calculateDistance(int x, int y) {
+		double dx = Math.abs(x - posX);
+		double dy = Math.abs(y - posY);
+		
+		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+	}
 }
